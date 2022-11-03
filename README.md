2cd256a7a4a2438247e83d9c491d5989c9a50d0d
Sudhir878786 <92601949+Sudhir878786@users.noreply.github.com>
2022-11-04 02:42:51 +0530

    Added Difficulties

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 ﻿# Codeforces-Problem-of-The-Day
  ## [live site](https://codeforces-probem-of-the-day.netlify.app/)
+ ## Responsive across all the devices
+
+![resp](https://user-images.githubusercontent.com/84634405/195657435-573cfab6-b954-48ee-b9ff-9bdc62d29a07.png)
  ## Gives a new problem everyday
-![image](https://user-images.githubusercontent.com/84634405/187019183-9f590032-c092-4249-9144-513e0777af28.png)
+
+![image](https://user-images.githubusercontent.com/84634405/195651847-10863575-5363-41cd-bcd3-d2afa9db379a.png)
+## Filter problem by difficulty
+![image](https://user-images.githubusercontent.com/84634405/195651671-c919cb42-e3fb-40e2-baab-0c79809649b3.png)
+![image](https://user-images.githubusercontent.com/84634405/195651689-640ef541-8ebd-4d75-a6a3-94654b7fffea.png)
+
 ## Click on the solve button to go to the probem
-![image](https://user-images.githubusercontent.com/84634405/187019220-3f3871c3-dcc9-46dc-a3ba-a27704eb8083.png)
+![image](https://user-images.githubusercontent.com/84634405/195653564-a0eeda04-302b-4daa-abca-3bb210bd4bd4.png)
+
 ## solve the problem
-![image](https://user-images.githubusercontent.com/84634405/187019223-6c10b15f-1042-41a8-bf8a-6e91a1040293.png)
+![image](https://user-images.githubusercontent.com/84634405/195653582-28de27f8-0188-4895-aaf7-63d6dcfc0206.png)
+
--- script.js
+++ script.js
@@ -1,34 +1,171 @@
-const newProblemButton = document.querySelector("button"),
-problemStatement = document.querySelector(".problem-statement p");
-function getNewProblem(){
-    newProblemButton.classList.add("dwell");
-    newProblemButton.innerText = "loading..";
-
-       console.log("Getting new Problem...");
-       let CodeForcesUrl = `https://codeforces.com/problemset/problem/1527/B1`;
-    window.open(CodeForcesUrl, "_blank");
-       // fetch("https://codeforces.com/api/probelmset.probelms?tags=implementation").then(newProblem => newProblem.json()).then(generatedNewProblem =>{
-        // console.log(generatedNewProblem);
+const solveButton = document.getElementById("solve-button")
+const problemStatement = document.querySelector(".problem-statement");
+const filterInput = document.getElementById("filter-rating");
+const contestId = document.getElementById("contestID");
+const problemID = document.getElementById("problemID");
+const problemRating = document.getElementById("problem-rating");
 
 
-        //newProblemButton.innerText = "New Problem";
-        
 
-    //})
-    newProblemButton.classList.remove("dwell");
+console.log("Trying to get a new Problem...");
+
+// global variable to store the problem URL.
+var problemURL;
 
 
-       
+/**
+ * If the current date is different than the date stored in localStorage, then update localStorage with
+ * the current date and return true. Otherwise, return false
+ * @returns a boolean value.
+ */
+function hasOneDayPassed() {
+	var date = new Date().toLocaleDateString();
+
+	if (localStorage.previousDate == date)
+		return false;
+
+	localStorage.previousDate = date;
+	return true;
 }
-function enterIntoTheProblem(){
-    problemStatement.classList.add("dwell");
-    problemStatement.innerText = "Taking you to CodeForces..";
-    console.log("Entering into CodeForces...");
-    problemStatement.classList.remove("dwell");
+
+/**
+ * It fetches the data from the codeforces api and returns the problems.
+ * @returns An array of objects.
+ */
+async function fetchCodeforcesProblemData() {
+	// using codeforces api to fetch problems
+	const response = await fetch('https://codeforces.com/api/problemset.problems');
+	const data = await response.json();
+
+	return data.result.problems;
+}
+
+/**
+ * It takes a list of problems and returns a random problem from the list
+ * @param problemList - an array of objects, each object contains the following properties:
+ * @returns The selected problem is being returned.
+ */
+function getOneProblem(problemList) {
+	const index = Math.floor(Math.random() * problemList.length);
+	const selectedProblem = problemList[index];
+
+	if (selectedProblem !== {} && selectedProblem !== undefined) {
+		// using localStorage to store the selected problem
+		localStorage.selectedProblem = JSON.stringify(selectedProblem);
+
+		// codeforces link is created and stored
+		problemURL = `https://codeforces.com/problemset/problem/${selectedProblem.contestId}/${selectedProblem.index}`;
+	}
+
+	return selectedProblem;
+}
+
+/**
+ * It updates the HTML elements on the page with the data from the selected problem.
+ * @param selectedProblem - The problem object that is selected from the list of problems.
+ */
+function updateHTML(selectedProblem) {
+	if (selectedProblem !== {} && selectedProblem !== undefined) {
+		// Updating the problem statement and problem details on the page
+		problemStatement.innerText = selectedProblem.name;
+		contestId.innerText = selectedProblem.contestId;
+		problemID.innerText = selectedProblem.index;
+		problemRating.innerText = selectedProblem.rating;
+
+		problemURL = `https://codeforces.com/problemset/problem/${selectedProblem.contestId}/${selectedProblem.index}`;
+	}
+}
+
+async function updateProblemByRating() {
+	// fetching input from filter
+	const filterRating = Number(filterInput.value);
+
+	// obtaining problem for the day from the list of problems
+	const problemList = await fetchCodeforcesProblemData();
+
+	// filtering problems based on rating
+	const filterProblemsList = problemList.filter(item => item.rating === filterRating);
+
+	// selecting a problem the list
+	const selectedProblem = getOneProblem(filterProblemsList);
+
+	updateHTML(selectedProblem);
+
+	// clearing filter input
+	filterInput.value = null;
+
+	console.log(selectedProblem);
 }
 
 
+// Updates the visitor counter
+async function updateVisitorCounter() {
+	try {
+		const isExistingVisitor = !!localStorage.getItem("isExistingVisitor");
 
-newProblemButton.addEventListener("click", getNewProblem);
-//newProblemButton.addEventListener("click", getNewProblem);
-// problemStatement.addEventListener("click", enterIntoTheProblem);
+		if (isExistingVisitor) {
+			const visitsResponse = await fetch("https://api.countapi.xyz/get/codeforces-probem-of-the-day.netlify.app/992ab16e-4a82-4d30-a834-a62af433f862");
+			const visits = await visitsResponse.json();
+			document.querySelector('#visitors > span.number').innerText = visits.value;
+		} else {
+			const visitsResponse = await fetch("https://api.countapi.xyz/hit/codeforces-probem-of-the-day.netlify.app/992ab16e-4a82-4d30-a834-a62af433f862");
+			const visits = await visitsResponse.json();
+			document.querySelector('#visitors > span.number').innerText = visits.value;
+			localStorage.setItem("isExistingVisitor", uuid())
+		}
+	} catch (error) {
+		console.error("Something went wrong. Trying to fetch last known number of visits again...");
+		const visitsResponse = await fetch("https://api.countapi.xyz/get/codeforces-probem-of-the-day.netlify.app/992ab16e-4a82-4d30-a834-a62af433f862");
+		const visits = await visitsResponse.json();
+		document.querySelector('#visitors > span.number').innerHTML = visits.value;
+	}
+}
+
+// Snippet from https://stackoverflow.com/a/2117523/10165585
+// Returns a UUID string
+function uuid() {
+	return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+		(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+	);
+}
+
+
+async function updateProblem() {
+	// checking if old problem data is existing, if exists display it.
+	if (localStorage.selectedProblem) {
+		// parsing object data from localstorage
+		const selectedProblem = localStorage.selectedProblem == undefined ? {} : JSON.parse(localStorage.selectedProblem);
+
+		updateHTML(selectedProblem);
+	}
+
+	// checking one day condition, if this check passes the new problem is fetched.
+	if (!hasOneDayPassed()) return false;
+
+	// obtaining problem for the day from the list of problems
+	const problemList = await fetchCodeforcesProblemData();
+
+	// selecting a problem the list
+	const selectedProblem = getOneProblem(problemList);
+
+	updateHTML(selectedProblem);
+
+	// console the problem details on every new problem fetch
+	console.log(selectedProblem);
+}
+
+// on every page load, it tries to update the problem
+window.addEventListener('DOMContentLoaded', updateProblem);
+
+// On every page load, it tries to update the visitor counter
+window.addEventListener('DOMContentLoaded', updateVisitorCounter);
+
+// keep checking every minute, and trying to load a new problem
+setInterval(updateProblem, 60000);
+
+function enterIntoTheProblem() {
+	window.open(problemURL, "_blank");
+}
+
+solveButton.addEventListener("click", enterIntoTheProblem);
+
